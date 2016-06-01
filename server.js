45eee692a53cf9913bd93dd05c66663b9b08faf3
Axel Schweiss <der.retter(at)web.de>
2016-06-01 16:32:29 +0200

    cors header access enabled

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ var PORT = process.env.PORT || 3000;
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Auth");
+    res.header('Access-Control-Expose-Headers', 'Auth');
     next();
 });
 
